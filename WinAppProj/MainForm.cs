9c3cac73f7517222dd808d1b654e23bdec75fbdb
pilarhz <p.michal18kun@gmail.com>
2026-04-21 19:49:32 +0200

    Test

---
+++ WinAppProj/MainForm.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace WinAppProj
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            this.Text = "Moja aplikacja";
+            this.Size = new Size(400, 300);
+
+            Button btn = new Button();
+            btn.Text = "Kliknij";
+            btn.Location = new Point(150, 120);
+
+            btn.Click += (s, e) => MessageBox.Show("Działa!");
+
+            this.Controls.Add(btn);
+        }
+    }
+}
